27ad1a324ed79d51f0aa2d68d77a32ab278111ca
NicMortelliti <npmortelliti@gmail.com>
2022-02-14 16:32:01 -0800

    commit tree 802a2ba3b5b5564391650d11eef2790ef0d35519

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {
+  // Return matching list of drivers, case insensitive
+  return array.filter(
+    (driver) => driver.toLowerCase() === string.toLowerCase()
+  );
+}
+
+function fuzzyMatch(array, string) {
+  // Return list of drivers whose names begin with the
+  // letters contained in 'string'
+  return array.filter((driver) => driver.startsWith(string));
+}
+
+function matchName(arrayObjects, string) {
+  // Each driver object contains 'name' and 'hometown'
+  // Return each element whose 'name' matches 'string'
+  return arrayObjects.filter((driver) => driver.name === string);
+}
